7a35c715b00e69b0771ec432367e1732551e1c2d
EckyZero <ebandersen87@gmail.com>
2014-08-29 10:11:38 -0500

    Added Holding Object

--- GDRun/GDRun/Holding.cs
+++ GDRun/GDRun/Holding.cs
@@ -4,10 +4,11 @@ namespace GDRun
 {
 	public class Holding
 	{
-		public Holding ()
-		{
-			 
-		}
+		public String Ticker 	{ get; set;}
+		public float Shares 	{ get; set;}
+		public float Price 		{ get; set;}
+		public float Value 		{ get; set;}
+		public float GainsNum	{ get; set;}
+		public float GainsPerc 	{ get; set;}
 	}
 }
-
